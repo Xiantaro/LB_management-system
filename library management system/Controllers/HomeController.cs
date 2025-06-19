a640763d3b329f24c37ddda0a21aa33e50029315
Xiann <ns45665412@gmail.com>
2025-06-19 21:38:12 +0800

    加入專案檔案。

---
+++ library management system/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+using System.Diagnostics;
+using library_management_system.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace library_management_system.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult BorrowBook()
+        {
+            return View();
+        }
+        public IActionResult ReturnTheBook()
+        {
+            return View();
+        }
+        public IActionResult AppointmentManagement()
+        {
+            return View();
+        }
+        public IActionResult BorrowingRecord()
+        {
+            return View();
+        }
+    }
+}
